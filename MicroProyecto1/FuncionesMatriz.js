8c463f7839b93720b2e767e32cd90c5ea4b2c2b8
Vincenzo <120345630+Vincenzo-ML@users.noreply.github.com>
2024-02-11 20:27:51 -0400

    Versión final
    Falto implementar responsividad para cambio de aparato

---
+++ MicroProyecto1/FuncionesMatriz.js
@@ -0,0 +1,304 @@
+const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,45,47,48,49,50];
+const playerScores = [0,0,0,0]
+
+document.addEventListener('DOMContentLoaded', () => {
+    const playerNames = JSON.parse(localStorage.getItem('playerNamesArray'));    
+    if (playerNames) {
+        printPlayerNames(playerNames);
+        changeText(JSON.parse(localStorage.getItem("playerNamesArray"))[0]);}
+    if (playerScores){
+        fillScoreTable(scores, playerNames)}});
+
+
+function createMatrix(n) {
+    return Array.from({ length: n }, () => Array.from({ length: n }, () => 0));
+  }
+  const matrix = createMatrix(3);
+  
+function llenarMatrizRandom(matrix) {
+    const list = Array.from({ length: 50 }, (_, i) => i + 1);
+
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+        const index = Math.floor(Math.random() * list.length);
+        const value = list[index];
+
+        list.splice(index, 1);
+
+        if (i < matrix.length && j < matrix[i].length) {
+            matrix[i][j] = value;
+        }
+        }
+    }
+
+    return matrix;
+}
+ 
+  
+  const numPlayers = localStorage.getItem("numPlayers")
+  const matrixSize = localStorage.getItem('boardSize');;
+  const arr = createMatrix(matrixSize);
+  
+  llenarMatrizRandom(arr);
+
+function createMatricesForPlayers(numPlayers, matrixSize) {
+    const matrices = [];
+  
+    for (let i = 0; i < numPlayers; i++) {
+      const matrix = createMatrix(matrixSize);
+      llenarMatrizRandom(matrix);
+      matrices.push(matrix);
+    }
+  
+    return matrices;
+  }
+
+matrices = createMatricesForPlayers(numPlayers, matrixSize)
+
+
+function updateTable(matrix) {
+    const table = document.getElementById("tblBingo");
+    table.innerHTML = "";
+  
+    for (let i = 0; i < matrix.length; i++) {
+      let tr = document.createElement("tr");
+      table.appendChild(tr);
+  
+      for (let j = 0; j < matrix[i].length; j++) {
+        let td = document.createElement("td");
+        td.id = matrix[i][j].toString();
+        td.style.height = "20%";
+        td.style.width = "20%";
+        td.classList.add("main-table-cell");
+  
+        let div = document.createElement("div");
+        div.classList.add("cell-format");
+  
+        if (matrix[i][j] === 0) {
+          div.style.backgroundColor = "red";
+        } else {
+          div.textContent = matrix[i][j].toString();
+        }
+  
+        td.appendChild(div);
+        tr.appendChild(td);
+      }
+    }
+  }
+
+
+let currentMatrixIndex = 0;
+const numMatrices = matrices.length;
+updateTable(matrices[0])
+
+
+function changeText(newText) {
+    const textBox = document.getElementById('customTextBox');
+    textBox.textContent = newText;
+}
+
+function changeMatrix(direction) {
+  currentMatrixIndex += direction;
+  if (currentMatrixIndex < 0) {
+    currentMatrixIndex = numMatrices - 1;
+  }
+  if (currentMatrixIndex >= numMatrices) {
+    currentMatrixIndex = 0;
+  }
+  updateTable(matrices[currentMatrixIndex]);
+  changeText(JSON.parse(localStorage.getItem("playerNamesArray"))[currentMatrixIndex]);
+}
+
+const leftArrowButton = document.querySelector(".left-arrow");
+leftArrowButton.addEventListener("click", () => {
+  changeMatrix(-1);
+});
+
+const rightArrowButton = document.querySelector(".right-arrow");
+rightArrowButton.addEventListener("click", () => {
+  changeMatrix(1);
+});
+
+
+
+
+function findNumberInMatrices(number, matrixIndex = -1) {
+    for (let i = 0; i < matrices.length; i++) {
+      for (let j = 0; j < matrices[i].length; j++) {
+        for (let k = 0; k < matrices[i][j].length; k++) {
+          if (matrices[i][j][k] === number) {
+            matrices[i][j][k] = 0;
+            if (matrixIndex !== -1) {
+              return matrixIndex;
+            }
+            return i;
+          }
+        }
+      }
+    }
+    return -1;
+  }
+  
+
+function checkAllMatrices() {
+    for (let matrixIndex = 0; matrixIndex < matrices.length; matrixIndex++) {
+        const matrix = matrices[matrixIndex];
+        checkDiagonal(matrix, matrixIndex);
+        checkBoard(matrix, matrixIndex);
+        checkcolumns(matrix, matrixIndex);
+        checkRows(matrix, matrixIndex) 
+        
+    }
+}
+
+const textBox = document.querySelector('.text-box .text');
+
+function generateNumber() {
+    if (numbers.length === 0) {
+      console.log("No se pueden generar más números");
+      return;
+    }
+    
+    const randomIndex = Math.floor(Math.random() * numbers.length);
+    const number = numbers[randomIndex];
+    
+    console.log(number);
+    
+    const matrixIndex = findNumberInMatrices(number);
+    if (matrixIndex !== -1) {
+      updateTable(matrices[matrixIndex]);
+      checkAllMatrices();
+    }
+    
+    textBox.textContent = number.toString();
+    numbers.splice(randomIndex, 1);
+  }
+  
+
+rotateWheelButton.addEventListener('click', generateNumber);
+
+
+function checkcolumns(matrix, matrixIndex) {
+    const rows = matrix.length;
+    const columns = matrix[0].length;
+  
+    for (let columnIndex = 0; columnIndex < columns; columnIndex++) {
+      let isColumnComplete = true;
+      for (let rowIndex = 0; rowIndex < rows; rowIndex++) {
+        if (matrix[rowIndex][columnIndex] !== 0) {
+          isColumnComplete = false;
+          break;
+        }
+      }
+      if (isColumnComplete) {
+        updatePlayerScore(matrixIndex, 1); 
+      }
+    }
+  }
+
+function checkRows(matrix, matrixIndex) {
+    const rows = matrix.length;
+    const columns = matrix[0].length;
+  
+    for (let rowIndex = 0; rowIndex < rows; rowIndex++) {
+      let isRowComplete = true;
+      for (let columnIndex = 0; columnIndex < columns; columnIndex++) {
+        if (matrix[rowIndex][columnIndex] !== 0) {
+          isRowComplete = false;
+          break;
+        }
+      }
+      if (isRowComplete) {
+        updatePlayerScore(matrixIndex, 1);
+      }
+    }
+  }
+
+
+function checkDiagonal(matrix, matrixIndex) {
+    const rows = matrix.length;
+    const columns = matrix[0].length;
+  
+    let isDiagonalComplete = true;
+    for (let i = 0; i < rows; i++) {
+      if (matrix[i][i] !== 0) {
+        isDiagonalComplete = false;
+        break;
+      }
+    }
+    if (isDiagonalComplete) {
+      updatePlayerScore(matrixIndex, 3); 
+    }
+  
+
+    isDiagonalComplete = true;
+    for (let i = 0; i < rows; i++) {
+      if (matrix[i][columns - i - 1] !== 0) {
+        isDiagonalComplete = false;
+        break;
+      }
+    }
+    if (isDiagonalComplete) {
+      updatePlayerScore(matrixIndex, 3); 
+    }
+  }
+
+
+function checkBoard(matrix) {
+    const rows = matrix.length;
+    const columns = matrix[0].length;
+  
+    for (let i = 0; i < rows; i++) {
+      let rowMarked = matrix[i].every((value) => value === 0);
+      if (!rowMarked) {
+        return false;
+      }
+    }
+  
+    for (let j = 0; j < columns; j++) {
+      let columnMarked = matrix.every((row) => row[j] === 0);
+      if (!columnMarked) {
+        return false;
+      }
+    }
+  
+    let diagonal1Marked = matrix.every((row, i) => row[i] === 0);
+    if (!diagonal1Marked) {
+      return false;
+    }
+  
+    let diagonal2Marked = matrix.every((row, i) => row[columns - i - 1] === 0);
+    if (!diagonal2Marked) {
+      return false;
+    }
+  
+    return true;
+  }
+  
+
+
+function updatePlayerScore(playerIndex, newScore) {
+    scores[playerIndex] += newScore;
+    const numPlayers = localStorage.getItem("numPlayers");
+    localStorage.setItem("playerScoresArray", JSON.stringify(scores.slice(0, numPlayers)));
+    fillScoreTable(scores.slice(0, numPlayers));}
+
+
+
+function findMaxScoreIndex(scores) {
+    let maxValue = scores[0];
+    let maxIndex = 0;
+  
+    for (let i = 1; i < scores.length; i++) {
+      if (scores[i] > maxValue) {
+        maxValue = scores[i];
+        maxIndex = i;
+      }
+    }
+  
+    return maxIndex;
+  }
+
+
+
+
--- MicroProyecto1/Functions.js
+++ MicroProyecto1/Functions.js
@@ -1,68 +1,199 @@
+const scores = [0,0,0,0];
 
 function getPlayerNames() {
-    const player1 = document.getElementById('player1').value;
-    const player2 = document.getElementById('player2').value;
-    const player3 = document.getElementById('player3').value;
-    const player4 = document.getElementById('player4').value;
+    const playerInputs = ['player1', 'player2', 'player3', 'player4'];
+    const playerNames = [];
+    let numPlayers = 0;
 
-    if (player1 && player2) {
-        localStorage.setItem('player1', player1);
-        localStorage.setItem('player2', player2);
-        localStorage.setItem('player3', player3);
-        localStorage.setItem('player4', player4);
-        document.getElementById('start-form').submit();
-        printPlayerNames();
-    } else {
-        alert('Por favor, ingrese al menos dos nombres de jugadores.');
-    }
-}
-
-
-function getPlayerNamesFromSessionStorage() {
-    const playerNames = sessionStorage.getItem('playerNames');
+    playerInputs.forEach(id => {
+      const input = document.getElementById(id);
+      if (input.value) {
+        if (playerNames.includes(input.value)) {
+          alert('Por favor, ingrese nombres de jugadores únicos.');
+          input.value = '';
+          input.focus();
+          return;
+        }
+        playerNames.push(input.value);
+        numPlayers++;
+      }
+    });
   
-    if (playerNames) {
-        return JSON.parse(playerNames);
+    if (numPlayers >= 2) {
+      localStorage.setItem('playerNamesArray', JSON.stringify(playerNames));
+      localStorage.setItem('boardSize', document.getElementById('boardSizeSelect').value);
+      localStorage.setItem('numPlayers', numPlayers);
+      document.getElementById('start-form').submit();
+      printPlayerNames(playerNames);
+      document.querySelector('table tbody').innerHTML = printPlayerNames();
     } else {
-        alert('No se encontraron jugadores en sessionStorage.');
-        return null;
+      alert('Por favor, ingrese al menos dos nombres de jugadores.');
     }
-}
-
-
-function startGame() {
-    const playerNames = getPlayerNamesFromSessionStorage();
-    const boardSize = document.querySelector('select').value;
   
-    if (playerNames) {
-        // Pasar los nombres de los jugadores y el tamaño del tablero al juego
-        window.location.href = `Juego.html?playerNames=${playerNames.join(',')}&boardSize=${boardSize}`;
-    } else {
-        alert('No se pudo iniciar la partida.');
-    }
-}
+    return playerNames;}
 
 
-function getGameParamsFromURL() {
-    const playerNames = JSON.parse(sessionStorage.getItem('playerNames'));
-    const boardSize = sessionStorage.getItem('boardSize');
-  
-    return { playerNames, boardSize };
-}
+function createZeroArray(numPlayers) {
+    return Array.from({ length: numPlayers }, () => 0);}
 
 
 function printPlayerNames(playerNames) {
+    let playerNamesHtml = '<tr><th>Nombre</th><th>Puntaje</th></tr>';
+    playerNames.forEach((playerName, index) => {
+        playerNamesHtml += `<tr><td>${index + 1}. ${playerName}</td><td>0</td></tr>`;
+    });
+    document.querySelector('table tbody').innerHTML = playerNamesHtml;
+}
+
+function getNumPlayersFromLocalStorage() {
+    const playerNamesArray = JSON.parse(localStorage.getItem('playerNamesArray'));
+    return playerNamesArray ? playerNamesArray.length : 0;
+}
+
+function printPlayerNames() {
+    const playerNames = JSON.parse(localStorage.getItem('playerNames'));
+    const roundNumberDiv = document.querySelector('.round-number');
+    let currentRoundNumber = parseInt(roundNumberDiv.textContent);
+    roundNumberDiv.textContent = currentRoundNumber + 1;
     console.log('Jugadores:');
     playerNames.forEach((playerName, index) => {
         console.log(`${index + 1}. ${playerName}`);
     });
 }
 
-
-const { playerNames, boardSize } = getGameParamsFromURL();
-
-if (playerNames) {
-    printPlayerNames(playerNames);
-} else {
-    alert('No se pudo obtener la información de los jugadores.');
+function startGame() {
+    const { playerNames, boardSize } = getGameParamsFromURL();
+    if (playerNames) {  
+        window.location.href = `Juego.html?playerNames=${playerNames.join(',')}&boardSize=${boardSize}`;
+        printPlayerNames(playerNames);  
+    } else {
+        alert('No se pudo iniciar la partida.');
+    }
 }
+
+function getGameParamsFromURL() {
+    const playerNames = JSON.parse(localStorage.getItem('playerNames'));
+    const boardSize = localStorage.getItem('boardSize');
+    return { playerNames, boardSize };
+}
+
+const rotateWheelButton = document.querySelector('.rotate-wheel-button');
+rotateWheelButton.addEventListener('click', () => {
+const roundNumberDiv = document.querySelector('.round-number');
+let currentRoundNumber = parseInt(roundNumberDiv.textContent);
+
+if (currentRoundNumber === 25) {
+    const maxScore = Math.max(...scores);
+    const isTie = scores.every(score => score === maxScore);
+if (isTie) {
+    alert('Final de partida! No hay ganadores.');
+    } else {
+      const maxScoreIndex = scores.indexOf(maxScore);
+      alert(`Final de partida! El ganador es: ${JSON.parse(localStorage.getItem("playerNamesArray"))[maxScoreIndex]}`);
+      addWinner(JSON.parse(localStorage.getItem("playerNamesArray"))[maxScoreIndex]);
+    }   
+    window.location.href = "index.html";
+    updateLeaderboard();
+    return;
+  }
+  roundNumberDiv.textContent = currentRoundNumber + 1;
+});
+
+let scoreTableRows;
+
+function fillScoreTable(playerScores) {
+  const tbody = document.querySelector('#score-table tbody');
+  tbody.innerHTML = '';
+  const headerRow = document.createElement('tr');
+  const scoreHeaderCell = document.createElement('th');
+  scoreHeaderCell.textContent = 'Puntaje';
+  headerRow.appendChild(scoreHeaderCell);
+  tbody.appendChild(headerRow);
+
+  scoreTableRows = [];
+
+  for (let i = 0; i < playerScores.length; i++) {
+    const row = document.createElement('tr');
+
+    const playerCell = document.createElement('td');
+    playerCell.textContent = JSON.parse(localStorage.getItem('playerNamesArray'))[i];
+    row.appendChild(playerCell);
+
+    const scoreCell = document.createElement('td');
+    scoreCell.textContent = playerScores[i];
+    row.appendChild(scoreCell);
+
+    tbody.appendChild(row);
+    scoreTableRows.push(row);
+  }
+}
+
+function addWinner(name) {
+    let winners = JSON.parse(localStorage.getItem('winners')) || [];
+    let existingWinner = winners.find(winner => winner.name === name);
+  
+    if (existingWinner) {
+      existingWinner.victories++;
+    } else {
+      winners.push({ name, victories: 1 });
+    }
+    localStorage.setItem('winners', JSON.stringify(winners));
+  }
+
+
+function updateLeaderboard() {
+    let winners = JSON.parse(localStorage.getItem('winners')) || [];
+    winners.sort((a, b) => b.victories - a.victories);
+    let tbody = document.querySelector('.leaderboard table tbody');
+    tbody.innerHTML = '';
+    winners.slice(0, 10).forEach(winner => {
+        let row = tbody.insertRow();
+        let nameCell = row.insertCell();
+        nameCell.textContent = winner.name;
+        let victoriesCell = row.insertCell();
+        victoriesCell.textContent = winner.victories;
+    });
+}
+
+function clearWinners() {
+    if (confirm('¿Estás seguro de que quieres eliminar todos los ganadores?')) {
+      localStorage.removeItem('winners');
+      let tbody = document.querySelector('.leaderboard table tbody');
+      tbody.innerHTML = '';
+      updateLeaderboard([]);
+    }
+  }
+  
+const clearWinnersButton = document.getElementById('clear-winners-button');
+clearWinnersButton.addEventListener('click', clearWinners);
+
+
+function showWinnersAlert() {
+    const winners = JSON.parse(localStorage.getItem('winners')) || [];
+    if (winners.length === 0) {
+      alert('No hay ganadores.');
+      return;
+    }
+  
+    let winnersMessage = 'Ganadores:\n';
+    updateLeaderboard();
+    winners.forEach((winner, index) => {
+      winnersMessage += `${index + 1}. ${winner.name} con ${winner.victories} victoria(s).\n`;
+    });
+  
+    alert(winnersMessage);
+  }
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const playerNames = getPlayerNames();
+    printPlayerNames(playerNames);
+    
+
+    
+});
+
+
+
+
